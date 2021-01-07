300efc281ce00f821c5614332a8abd0d22810831
Nohossat TRAORE <nohossat.traore@gmail.com>
2021-01-07 12:41:33 +0100

    add print

--- scrap.py
+++ scrap.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import pandas as pd
 
+print("c nono")
+
 url='https://en.wikipedia.org/wiki/List_of_countries_by_intentional_homicide_rate'#Create a handle, page, to handle the contents of the website
 soup = BeautifulSoup(url,'lxml')#Store the contents of the website under doc
 print(soup.prettify())
